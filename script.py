aa9f90d7d4af7c482c81bfe4abb53d94c239124e
babylonQ <mirzad@gmail.com>
2016-07-14 14:26:05 +0200

    WAF on/off

--- script.py
+++ script.py
@@ -7,28 +7,53 @@ password = 'password'
 with open("/root/Desktop/payloads") as f:
 	payloads = f.read().splitlines()
 
-sufix = "&Submit=Submit#"
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+opener2 = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
+def getFlag():
+        if flag == 0:
+                print "WAF: OFF"
+                return;
+        else:
+                print "WAF: ON"
+                return;
+
+
+sufix = "&Submit=Submit#"
+flag = 0
 menu = {}
+menu['0']="Set WAF"
 menu['1']="SQLi"
 menu['2']="XSS"
 menu['3']="Exit"
 while True:
   options=menu.keys()
   options.sort()
+
+  getFlag()
+
   for entry in options:
 	print entry, menu[entry]
 
   #Menu options
   selection = raw_input("Please select:")
-  if selection == '1':
-	cj = cookielib.CookieJar()
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-	opener2 = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+  if selection == '0':
+	if flag == 0:
+		opener.open('http://192.168.57.100/index.php')
+		opener2.open('http://192.168.57.100/security.php?phpids=on')
+		flag = 1
+	else: 
+		opener.open('http://192.168.57.100/index.php')
+                opener2.open('http://192.168.57.100/security.php?phpids=off')
+                flag = 0
 
+  if selection == '1':
+#	cj = cookielib.CookieJar()
+#	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 #        login_data = urllib.urlencode({'username' : username, 'password' : password})
-        opener.open('http://192.168.57.100/index.php')
-	opener2.open('http://192.168.57.100/security.php?phpids=on')
+#        opener.open('http://192.168.57.100/index.php')
+#	opener2.open('http://192.168.57.100/security.php?phpids=on')
 	# Test each payload from the file
 	count = 0
 	for payload in payloads:
@@ -50,7 +75,6 @@ while True:
   elif selection == '3':
 	break
   else:
-	print "Unknown option selected!"
-
-
+	print ""
 #print resp.read()
+
